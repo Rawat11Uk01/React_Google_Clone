440d5e4584437d17119aba140fa02034c06121d2
Rawat11Uk01 <kashishrawat308@gmail.com>
2022-04-14 12:44:40 +0530

    first commit

--- src/componets/SearchPage.js
+++ src/componets/SearchPage.js
@@ -24,43 +24,43 @@ function SearchPage() {
           <img className='searchPage_logo' src='https://1000logos.net/wp-content/uploads/2016/11/google-logo.jpg' />
         </Link>
         <div className='search_header_body'>
-          <Search hideButtons/>
+          <Search hideButtons />
           <div className='searchPage_options'>
             <div className='searchPage_optionsLeft'>
-             <div className='searchPage_option'>
-               <SearchIcon/>
-               <Link to='/all'>All</Link>
-             </div>
-             <div className='searchPage_option'>
-               <DescriptionIcon/>
-               <Link to='/news'>News</Link>
-             </div>
-             <div className='searchPage_option'>
-               <ImageIcon/>
-               <Link to='/images'>Images</Link>
-             </div>
-             <div className='searchPage_option'>
-               <LocalOfferIcon/>
-               <Link to='/shopping'>Shopping</Link>
-             </div>
-             <div className='searchPage_option'>
-               <RoomIcon/>
-               <Link to='/maps'>Maps</Link>
-             </div>
-             <div className='searchPage_option'>
-               <MoreVertIcon/>
-               <Link to='/more'>more</Link>
-             </div>
+              <div className='searchPage_option'>
+                <SearchIcon />
+                <Link to='/all'>All</Link>
+              </div>
+              <div className='searchPage_option'>
+                <DescriptionIcon />
+                <Link to='/news'>News</Link>
+              </div>
+              <div className='searchPage_option'>
+                <ImageIcon />
+                <Link to='/images'>Images</Link>
+              </div>
+              <div className='searchPage_option'>
+                <LocalOfferIcon />
+                <Link to='/shopping'>Shopping</Link>
+              </div>
+              <div className='searchPage_option'>
+                <RoomIcon />
+                <Link to='/maps'>Maps</Link>
+              </div>
+              <div className='searchPage_option'>
+                <MoreVertIcon />
+                <Link to='/more'>more</Link>
+              </div>
             </div>
             <div className='searchPage_optionsRight'>
-            <div className='searchPage_option'>
-               
-               <Link to='/settings'>Settings</Link>
-             </div>
-             <div className='searchPage_option'>
-               
-               <Link to='/tools'>Tools</Link>
-             </div>
+              <div className='searchPage_option'>
+
+                <Link to='/settings'>Settings</Link>
+              </div>
+              <div className='searchPage_option'>
+
+                <Link to='/tools'>Tools</Link>
+              </div>
             </div>
           </div>
         </div>
